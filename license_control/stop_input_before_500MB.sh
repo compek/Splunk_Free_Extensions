7c54c5c2dc2a84ac43fbd748208d5b1d594b1345
compek <compek@gmail.com>
2024-06-09 12:10:03 +0200

    Update stop_input_before_500MB.sh

--- license_control/stop_input_before_500MB.sh
+++ license_control/stop_input_before_500MB.sh
@@ -1,3 +1,7 @@
-index=_internal earliest=-1@d sourcetype=splunkd source=*license_usage.log type=Usage
-| eval MB_Used = b / 1024 / 1024
-| stats sum(MB_Used)
+#!/bin/bash
+STOP_AT=490000000
+BYTES=$(/opt/splunk/bin/splunk search ' index=_internal earliest=@d sourcetype=splunkd source=*license_usage.log type=Usage |stats sum(b)' 2>/dev/null | tail -n 1)
+if [[ $BYTES >= $STOP_AT]]; then
+  # splunk edit monitor your_input_name -disabled true
+  # splunk disable listen port
+fi
